a4cba50025d2fd790b6c31d769bf6d19ae1a7f05
jose <jose@world.com>
2025-09-09 12:55:25 -0400

    chapter 3

--- ch03_Jose_Acuna.js
+++ ch03_Jose_Acuna.js
@@ -2,7 +2,7 @@
 // Author: Jose Acuna
 // Class: Web Programming
 
-//1.0 Minimun
+//3.1 Minimun
 function min(a, b) { //Create a function call min and 
   if (a < b) { //Compared "a" and "b". If "a" is greater than "b"
     return a; //Show "a" if "a" is greater than "b"
@@ -13,3 +13,20 @@ function min(a, b) { //Create a function call min and
 // Ex:
 console.log(min(5, 10));
 console.log(min(20, 7));
+
+//3.2 Recursion
+function isEven(n) { // Function that differentiates between even and odd numbers
+  if (n === 0) {
+    return true;   // if n is equal to 0 is even
+  } else if (n === 1) {
+    return false;  // if n is equal to 1 is odd
+  } else if (n < 0) {
+    return isEven(-n); // Corrects negative numbers, so negative by negative would give a positive number
+  } else {
+    return isEven(n - 2); // caso recursivo
+  }
+}
+console.log(isEven(50));  // true
+console.log(isEven(75));  // false
+console.log(isEven(-1));  // false
+
